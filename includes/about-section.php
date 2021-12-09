5dfdcbb1dc26962bf58cfdc68e16dd1825ead1ad
lacbainnel <j.lacbain@yahoo.com>
2021-12-10 07:13:33 +0800

    first commit

---
+++ includes/about-section.php
@@ -0,0 +1,38 @@
+<div class="row">   
+<div class="col-6">
+    <h1>A TRUE LEADER CAN HELP YOU IN BUSINESS</h1>
+    <p>
+        Lorem ipsum dolor sit amet, an labores explicari qui, eu nostrum 
+        copiosae argumentum has. Latine propriae quo no, unum ridens expetenda 
+        id sit, at usu eius eligendi singulis. Sea ocurreret principes ne.
+    </p>
+
+    <div class="container">
+    <h6>Business Management</h6>
+    <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+        <span class="sr-only">70% Complete</span>
+        </div>
+    </div>
+
+    <h6>Financial Management</h6>
+    <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+        <span class="sr-only">70% Complete</span>
+        </div>
+    </div>
+
+    <h6>Project Management</h6>
+    <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+        <span class="sr-only">70% Complete</span>
+        </div>
+    </div>
+    </div>
+</div>
+<div class="col-6">
+    <img class="img-fluid" src="css/slider.jpg" alt="">
+</div>
+</div>
+
+
